10eba8b4b6ae8d4122368758c5fac8470e718257
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2023-04-20 19:49:39 -0300

    Adding Tenant Id to the well-known URL

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,7 +7,7 @@ export const authOptions = {
             issuer: process.env.FUSIONAUTH_ISSUER,
             clientId: process.env.FUSIONAUTH_CLIENT_ID,
             clientSecret: process.env.FUSIONAUTH_CLIENT_SECRET,
-            wellKnown: `${process.env.FUSIONAUTH_URL}/.well-known/openid-configuration`,
+            wellKnown: `${process.env.FUSIONAUTH_URL}/.well-known/openid-configuration/${process.env.FUSIONAUTH_TENANT_ID}`,
             tenantId: process.env.FUSIONAUTH_TENANT_ID, // Only required if you're using multi-tenancy
         }),
     ],
